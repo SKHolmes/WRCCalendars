506822ab6920f29177dc259d76ecc3470ecfd7ce
SKHolmes <samualkholmes@gmail.com>
2017-11-13 22:25:34 +1300

    Added ordering functionality go to /orders to DL orders.

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var app = express();
 var nodemailer = require("nodemailer");
 var smtpTransport = require('nodemailer-smtp-transport');
 var router = express.Router();
+var fs = require("fs");
 
 var transport = nodemailer.createTransport({
-  service: 'gmail',
+	service: 'gmail',
   //secureConnection: false, // use SSL
   //port: 587, // port for secure SMTP
   auth: {
-    user: 'wrccalendars@gmail.com',
-    pass: 'ilikepie123'
+  	user: 'wrccalendars@gmail.com',
+  	pass: 'ilikepie123'
   }
 });
 
@@ -24,6 +25,11 @@ app.set('view engine', 'ejs');
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
+app.get('/orders', function(req, res){
+	var file = __dirname + '/public/orders.csv';
+  res.download(file); // Set disposition and send it.
+});
+
 // set the home page route
 app.get('/', function(req, res) {
     // ejs render automatically looks in the views folder
@@ -34,10 +40,31 @@ app.get('/send',function(req,res){
 	var mailOptions={
 		from : 'wrccalendars@gmail.com',
 		to:'wrccommunications01@gmail.com',
+		//to:'samualkholmes@gmail.com',
 		subject : req.query.subject,
 		text : req.query.text
 	}
-	console.log(mailOptions);
+	//console.log(mailOptions);
+	fs.readFile(__dirname + '/public/id.txt', 'utf8', function(error, data){
+		if(error){
+			return console.log(error);
+		}
+		var id = parseInt(data);
+		var order = id + "," + req.query.name + "," + req.query.calNum + "," + req.query.address + "," + req.query.email + "," + req.query.number + "," + req.query.delOrPic + ",No,\n";
+		fs.appendFile(__dirname + '/public/orders.csv', order, 'utf8', function(error, data){
+			if(error){
+				return console.log("Error writing order: "+order);
+			}
+		});
+
+		
+		fs.writeFile(__dirname + '/public/id.txt', (""+(id+1)), 'utf8', function(error, data){
+			if(error){
+				return console.log("Error writing ID.");
+			}
+		});
+	});
+
 	transport.sendMail(mailOptions, function(error, response){
 		if(error){
 			console.log(error);
